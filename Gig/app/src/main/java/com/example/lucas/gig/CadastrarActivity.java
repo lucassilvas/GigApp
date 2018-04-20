40356664d3693209b7aa3dfe205bfa8c5a607542
Lucas Silva <luskasilvas@gmail.com>
2018-04-20 19:52:52 -0300

    att

--- Gig/app/src/main/java/com/example/lucas/gig/CadastrarActivity.java
+++ Gig/app/src/main/java/com/example/lucas/gig/CadastrarActivity.java
@@ -1,8 +1,11 @@
 package com.example.lucas.gig;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -15,14 +18,66 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class CadastrarActivity extends AppCompatActivity {
+public class CadastrarActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private FirebaseAuth firebaseAuth;
+    private EditText email;
+    private EditText senha;
+    private Button continuar;
+    ProgressDialog progressLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar);
 
-
+        firebaseAuth = FirebaseAuth.getInstance();
+        email = (EditText) findViewById(R.id.email);
+        senha = (EditText) findViewById(R.id.senha);
+        continuar = (Button) findViewById(R.id.continuar);
+        progressLogin = new ProgressDialog(this); // telazinha que
+        continuar.setOnClickListener(this);
     }
 
+
+    //função para registrar um usuário
+    private void registerUser(){
+        //converte o edit text para string.
+        String login = email.getText().toString().trim();
+        String password  = senha.getText().toString().trim();
+        //verifica se os campos estão vazios
+        if(TextUtils.isEmpty(login)){
+            Toast.makeText(this, "Entre com seu email",Toast.LENGTH_LONG).show();
+            return;
+        }
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(this, "Entre com sua senha",Toast.LENGTH_LONG).show();
+            return;
+        }
+        progressLogin.setMessage("Registrando Usuário...");
+        progressLogin.show();
+        //função para adicionar no banco
+        firebaseAuth.createUserWithEmailAndPassword(login, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(CadastrarActivity.this, "Sucesso",Toast.LENGTH_LONG).show();
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            intent.putExtra("nome", email.getText().toString().trim());
+                            startActivity(intent);
+                        }else{
+                            Toast.makeText(CadastrarActivity.this, "Erro",Toast.LENGTH_LONG).show();
+                        }
+                        progressLogin.dismiss();
+                    }
+                });
+    }
+
+    //chama o a função para registrar um usuário
+    @Override
+    public void onClick(View v) {
+        registerUser();
+    }
 }
--- Gig/app/src/main/java/com/example/lucas/gig/MainActivity.java
+++ Gig/app/src/main/java/com/example/lucas/gig/MainActivity.java
@@ -1,26 +1,47 @@
 package com.example.lucas.gig;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+    private EditText mEmailField;
+    private EditText mPasswordField;
+    private static final String TAG = "EmailPassword";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mEmailField = findViewById(R.id.btn_usuario);
+        mPasswordField = findViewById(R.id.senha);
+
         //Abrir bandaActivity ao clicar no botão login
         Button btn_login = (Button) findViewById(R.id.btn_login);
         btn_login.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                Intent bandaActivity = new Intent(MainActivity.this, BandaActivity.class);
-                startActivity(bandaActivity);
+                int i = view.getId();
+                if(i == R.id.btn_login){
+                    signIn(mEmailField.getText().toString(), mPasswordField.getText().toString());
+                }
+
             }
         });
 
@@ -32,8 +53,57 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(cadastrarActivity);
             }
         });
+
+        mAuth = FirebaseAuth.getInstance();
+
+
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void signIn(String email, String password){
+        Log.d(TAG, "signIn:" + email);
+        //verifica se os campos estão vazios
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(this, "Entre com seu email",Toast.LENGTH_LONG).show();
+            return;
+        }
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(this, "Entre com sua senha",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent bandaActivity = new Intent(MainActivity.this, BandaActivity.class);
+                            startActivity(bandaActivity);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                    }
+                });
+    }
+
+    private void updateUI(FirebaseUser currentUser) {
+
+    }
 
 
 }
