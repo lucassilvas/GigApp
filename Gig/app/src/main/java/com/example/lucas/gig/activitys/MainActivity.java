e049f36d16e090a6c390df1628f85681ff509442
franca.willi <franca.willi@gmail.com>
2018-05-14 19:30:25 -0300

    Att

--- Gig/app/src/main/java/com/example/lucas/gig/activitys/MainActivity.java
+++ Gig/app/src/main/java/com/example/lucas/gig/activitys/MainActivity.java
@@ -1,5 +1,5 @@
 package com.example.lucas.gig.activitys;
-//IMPORTS
+//IMPORT
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
