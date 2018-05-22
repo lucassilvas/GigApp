49121ff8052d255a1f14e4c7619ac7e5f00a0fce
franca-william <franca.willi@gmail.com>
2018-05-21 21:27:22 -0300

    Att 21/05/18
    Atualização CriarEventoActivity

--- Gig/app/src/main/java/com/example/lucas/gig/activitys/CriarEventoActivity.java
+++ Gig/app/src/main/java/com/example/lucas/gig/activitys/CriarEventoActivity.java
@@ -1,5 +1,5 @@
 package com.example.lucas.gig.activitys;
-
+//AJEITAR A DESCRICAO COLOCANDO COMO UMA CAIXA DE TEXTO
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
