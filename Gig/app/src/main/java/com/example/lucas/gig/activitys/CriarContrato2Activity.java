b599b7fb36b0e3e391838099e2b6613b7fc642f9
franca-william <franca.willi@gmail.com>
2018-05-28 20:12:40 -0300

    Att
    Mudança no nome das activitys

--- Gig/app/src/main/java/com/example/lucas/gig/activitys/CriarContrato2Activity.java
+++ Gig/app/src/main/java/com/example/lucas/gig/activitys/CriarContrato2Activity.java
@@ -11,5 +11,7 @@ public class CriarContrato2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_criar_contrato2);
+
+
     }
 }
--- Gig/app/src/main/java/com/example/lucas/gig/activitys/CriarContratoActivity.java
+++ Gig/app/src/main/java/com/example/lucas/gig/activitys/CriarContratoActivity.java
@@ -4,9 +4,16 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.lucas.gig.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.mikepenz.fontawesome_typeface_library.FontAwesome;
 import com.mikepenz.materialdrawer.AccountHeader;
 import com.mikepenz.materialdrawer.AccountHeaderBuilder;
@@ -18,10 +25,37 @@ import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 public class CriarContratoActivity extends AppCompatActivity {
     private Drawer result;
 
+    private EditText nomeCompleto;
+    private EditText cidade;
+    private EditText estado;
+    private EditText rua;
+    private EditText numero;
+    private EditText bairro;
+    private EditText cpf;
+    private EditText rg;
+    private EditText nacionalidade;
+    private EditText estadoCivil;
+    private Button continuar;
+    private Button cancelar;
+    private FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_criar_contrato);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        //ATRIBUINDO OS VALORES REFERENTES AS ENTRADAS DO USUÁRIO
+        nomeCompleto    =  findViewById(R.id.nomeCompleto);
+        cidade          =  findViewById(R.id.cidade);
+        estado          =  findViewById(R.id.estado);
+        rua             =  findViewById(R.id.rua);
+        numero          =  findViewById(R.id.numero);
+        bairro          =  findViewById(R.id.bairro);
+        cpf             =  findViewById(R.id.cpf);
+        rg              =  findViewById(R.id.rg);
+        nacionalidade   =  findViewById(R.id.nacionalidade);
+        estadoCivil     =  findViewById(R.id.estadoCivil);
+
 
         //cria a barra de toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -72,6 +106,61 @@ public class CriarContratoActivity extends AppCompatActivity {
         result.addStickyFooterItem(new PrimaryDrawerItem().withName("Sair"));
     }
 
+
+    //FUNÇÃO QUE PEGA OS VALORES DO USUÁRIO E SALVA EM VARIÁVEIS
+    private void criarContrato(){
+        // CONVERTENDO OS VALORES DOS EDITTEXTS EM STRING
+        final String nomeCompletoStr = nomeCompleto.getText().toString();
+        final String cidadeStr = cidade.getText().toString().trim();
+        final String estadoStr = estado.getText().toString().trim();
+        final String ruaStr = rua.getText().toString();
+        final String numeroStr = numero.getText().toString().trim();
+        final String bairroStr = bairro.getText().toString();
+        final String cpfStr = cpf.getText().toString().trim();
+        final String rgStr= rg.getText().toString().trim();
+        final String nacionalidadeStr = nacionalidade.getText().toString().trim();
+        final String estadoCivilStr = estadoCivil.getText().toString().trim();
+
+
+        //VERIFICA SE ALGUM CAMPO ESTÁ VAZIO
+        if(TextUtils.isEmpty(nomeCompletoStr)       ||
+                TextUtils.isEmpty(cidadeStr)        ||
+                TextUtils.isEmpty(estadoStr)        ||
+                TextUtils.isEmpty(ruaStr)           ||
+                TextUtils.isEmpty(numeroStr)        ||
+                TextUtils.isEmpty(bairroStr)        ||
+                TextUtils.isEmpty(cpfStr)           ||
+                TextUtils.isEmpty(rgStr)            ||
+                TextUtils.isEmpty(nacionalidadeStr) ||
+                TextUtils.isEmpty(estadoCivilStr)){
+            Toast.makeText(this, "Preencha todos os campos !!",Toast.LENGTH_LONG).show();
+            return;
+        }
+        //CHAMADA À FUNÇÃO DE CADASTRO NO BANCO
+        cadastrarNovoContratoBD(nomeCompletoStr,cidadeStr,estadoStr,ruaStr,numeroStr,bairroStr,cpfStr,rgStr,nacionalidadeStr,estadoCivilStr);
+    }
+
+
+    //FUNÇÃO QUE REGISTRA O CONTRATO NO BANCO
+    private void cadastrarNovoContratoBD(String nomeCompleto, String cidade, String estado, String rua, String numero, String bairro, String cpf, String rg, String nacionalidade, String estadoCivil){
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference().child("contratos");
+        DatabaseReference currentUserDB = mDatabase.child(firebaseAuth.getCurrentUser().getUid());
+        currentUserDB.child("nomeCompleto").setValue(nomeCompleto);
+        currentUserDB.child("cidade").setValue(nomeCompleto);
+        currentUserDB.child("estado").setValue(nomeCompleto);
+        currentUserDB.child("rua").setValue(nomeCompleto);
+        currentUserDB.child("numero").setValue(nomeCompleto);
+        currentUserDB.child("bairro").setValue(nomeCompleto);
+        currentUserDB.child("cpf").setValue(nomeCompleto);
+        currentUserDB.child("rg").setValue(nomeCompleto);
+        currentUserDB.child("nacionalidade").setValue(nomeCompleto);
+        currentUserDB.child("estadoCivil").setValue(nomeCompleto);
+
+
+    }
+    public void onClick(View v) {
+        criarContrato();
+    }
 
 }
 
