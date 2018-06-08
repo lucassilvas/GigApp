1b3bbba245e2e417d7c8ed7c66e7483139406233
franca-william <franca.willi@gmail.com>
2018-06-08 14:17:54 -0300

    Att Contrato
    Falta só os botões, Scroll View OK

--- Gig/app/src/main/java/com/example/lucas/gig/activitys/CriarContratoActivity.java
+++ Gig/app/src/main/java/com/example/lucas/gig/activitys/CriarContratoActivity.java
@@ -26,15 +26,22 @@ public class CriarContratoActivity extends AppCompatActivity {
     private Drawer result;
 
     private EditText nomeCompleto;
-    private EditText cidade;
-    private EditText estado;
-    private EditText rua;
-    private EditText numero;
-    private EditText bairro;
     private EditText cpf;
     private EditText rg;
-    private EditText nacionalidade;
     private EditText estadoCivil;
+    private EditText nacionalidade;
+    private EditText estado;
+    private EditText cidade;
+    private EditText rua;
+    private EditText bairro;
+    private EditText numero;
+    private EditText nomeBanda;
+    private EditText duracaoEvento;
+    private EditText dia;
+    private EditText horaInicio;
+    private EditText horaTermino;
+    private EditText pagamento;
+    private EditText condicoes;
     private Button continuar;
     private Button cancelar;
     private FirebaseAuth firebaseAuth;
@@ -46,15 +53,22 @@ public class CriarContratoActivity extends AppCompatActivity {
 
         //ATRIBUINDO OS VALORES REFERENTES AS ENTRADAS DO USUÁRIO
         nomeCompleto    =  findViewById(R.id.nomeCompleto);
-        cidade          =  findViewById(R.id.cidade);
-        estado          =  findViewById(R.id.estado);
-        rua             =  findViewById(R.id.rua);
-        numero          =  findViewById(R.id.numero);
-        bairro          =  findViewById(R.id.bairro);
         cpf             =  findViewById(R.id.cpf);
         rg              =  findViewById(R.id.rg);
-        nacionalidade   =  findViewById(R.id.nacionalidade);
         estadoCivil     =  findViewById(R.id.estadoCivil);
+        nacionalidade   =  findViewById(R.id.nacionalidade);
+        estado          =  findViewById(R.id.estado);
+        cidade          =  findViewById(R.id.cidade);
+        rua             =  findViewById(R.id.rua);
+        bairro          =  findViewById(R.id.bairro);
+        numero          =  findViewById(R.id.numero);
+        nomeBanda       =  findViewById(R.id.nomeBanda);
+        duracaoEvento   =  findViewById(R.id.duracaoEvento);
+        dia             =  findViewById(R.id.diaEvento);
+        horaInicio      =  findViewById(R.id.horaInicio);
+        horaTermino     =  findViewById(R.id.horaTermino);
+        pagamento       =  findViewById(R.id.pagamento);
+        condicoes       =  findViewById(R.id.condicoes);
 
 
         //cria a barra de toolbar
@@ -110,16 +124,24 @@ public class CriarContratoActivity extends AppCompatActivity {
     //FUNÇÃO QUE PEGA OS VALORES DO USUÁRIO E SALVA EM VARIÁVEIS
     private void criarContrato(){
         // CONVERTENDO OS VALORES DOS EDITTEXTS EM STRING
-        final String nomeCompletoStr = nomeCompleto.getText().toString();
-        final String cidadeStr = cidade.getText().toString().trim();
-        final String estadoStr = estado.getText().toString().trim();
-        final String ruaStr = rua.getText().toString();
-        final String numeroStr = numero.getText().toString().trim();
-        final String bairroStr = bairro.getText().toString();
-        final String cpfStr = cpf.getText().toString().trim();
-        final String rgStr= rg.getText().toString().trim();
-        final String nacionalidadeStr = nacionalidade.getText().toString().trim();
-        final String estadoCivilStr = estadoCivil.getText().toString().trim();
+        final String nomeCompletoStr    = nomeCompleto.getText().toString();
+        final String cidadeStr          = cidade.getText().toString().trim();
+        final String estadoStr          = estado.getText().toString().trim();
+        final String ruaStr             = rua.getText().toString();
+        final String numeroStr          = numero.getText().toString().trim();
+        final String bairroStr          = bairro.getText().toString();
+        final String cpfStr             = cpf.getText().toString().trim();
+        final String rgStr              = rg.getText().toString().trim();
+        final String nacionalidadeStr   = nacionalidade.getText().toString().trim();
+        final String estadoCivilStr     = estadoCivil.getText().toString().trim();
+        final String nomeBandaStr       = nomeBanda.getText().toString().trim();
+        final String duracaoEventoStr   = duracaoEvento.getText().toString().trim();
+        final String diaStr             = dia.getText().toString().trim();
+        final String horaInicioStr      = horaInicio.getText().toString().trim();
+        final String horaTerminoStr     = horaTermino.getText().toString().trim();
+        final String pagamentoStr       = pagamento.getText().toString().trim();
+        final String condicoesStr       = condicoes.getText().toString().trim();
+
 
 
         //VERIFICA SE ALGUM CAMPO ESTÁ VAZIO
@@ -132,29 +154,45 @@ public class CriarContratoActivity extends AppCompatActivity {
                 TextUtils.isEmpty(cpfStr)           ||
                 TextUtils.isEmpty(rgStr)            ||
                 TextUtils.isEmpty(nacionalidadeStr) ||
-                TextUtils.isEmpty(estadoCivilStr)){
+                TextUtils.isEmpty(estadoCivilStr)   ||
+                TextUtils.isEmpty(nomeBandaStr)     ||
+                TextUtils.isEmpty(duracaoEventoStr) ||
+                TextUtils.isEmpty(diaStr)           ||
+                TextUtils.isEmpty(horaInicioStr)    ||
+                TextUtils.isEmpty(horaTerminoStr)   ||
+                TextUtils.isEmpty(pagamentoStr)     ||
+                TextUtils.isEmpty(condicoesStr)){
             Toast.makeText(this, "Preencha todos os campos !!",Toast.LENGTH_LONG).show();
             return;
         }
         //CHAMADA À FUNÇÃO DE CADASTRO NO BANCO
-        cadastrarNovoContratoBD(nomeCompletoStr,cidadeStr,estadoStr,ruaStr,numeroStr,bairroStr,cpfStr,rgStr,nacionalidadeStr,estadoCivilStr);
+        cadastrarNovoContratoBD(nomeCompletoStr,cidadeStr,estadoStr,ruaStr,numeroStr,bairroStr,cpfStr,rgStr,nacionalidadeStr,estadoCivilStr,nomeBandaStr,duracaoEventoStr,diaStr,horaInicioStr,horaTerminoStr,pagamentoStr,condicoesStr);
     }
 
 
     //FUNÇÃO QUE REGISTRA O CONTRATO NO BANCO
-    private void cadastrarNovoContratoBD(String nomeCompleto, String cidade, String estado, String rua, String numero, String bairro, String cpf, String rg, String nacionalidade, String estadoCivil){
+    private void cadastrarNovoContratoBD(String nomeCompleto, String cidade, String estado, String rua, String numero, String bairro, String cpf, String rg, String nacionalidade, String estadoCivil, String nomeBanda, String duracaoEvento, String dia, String horaInicio, String horaTermino, String pagamento, String condicoes){
         DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference().child("contratos");
         DatabaseReference currentUserDB = mDatabase.child(firebaseAuth.getCurrentUser().getUid());
         currentUserDB.child("nomeCompleto").setValue(nomeCompleto);
-        currentUserDB.child("cidade").setValue(nomeCompleto);
-        currentUserDB.child("estado").setValue(nomeCompleto);
-        currentUserDB.child("rua").setValue(nomeCompleto);
-        currentUserDB.child("numero").setValue(nomeCompleto);
-        currentUserDB.child("bairro").setValue(nomeCompleto);
-        currentUserDB.child("cpf").setValue(nomeCompleto);
-        currentUserDB.child("rg").setValue(nomeCompleto);
-        currentUserDB.child("nacionalidade").setValue(nomeCompleto);
-        currentUserDB.child("estadoCivil").setValue(nomeCompleto);
+        currentUserDB.child("cidade").setValue(cidade);
+        currentUserDB.child("estado").setValue(estado);
+        currentUserDB.child("rua").setValue(rua);
+        currentUserDB.child("numero").setValue(numero);
+        currentUserDB.child("bairro").setValue(bairro);
+        currentUserDB.child("estadoCivil").setValue(estadoCivil);
+        currentUserDB.child("cpf").setValue(cpf);
+        currentUserDB.child("rg").setValue(rg);
+        currentUserDB.child("nacionalidade").setValue(nacionalidade);
+        currentUserDB.child("nomeBanda").setValue(nomeBanda);
+        currentUserDB.child("duracaoEvento").setValue(duracaoEvento);
+        currentUserDB.child("dia").setValue(dia);
+        currentUserDB.child("horaInicio").setValue(horaInicio);
+        currentUserDB.child("horaTermino").setValue(horaTermino);
+        currentUserDB.child("pagamento").setValue(pagamento);
+        currentUserDB.child("condicoes").setValue(condicoes);
+
+
 
 
     }
